e7d1685f233f2ac38c8fe9967304c0af5ba0096a
pascal-vegt <9016712@student.zadkine.nl>
2024-06-14 18:09:53 +0200

    some styling done

---
+++ app.js
@@ -0,0 +1,4 @@
+//<div class="main-categories-section-list-item">
+//<img class="main-categories-section-list-item-image" src="./images/566736.avif" alt="test image">
+//<h1 class="main-categories-section-list-item-title">Name</h1>
+//</div>
