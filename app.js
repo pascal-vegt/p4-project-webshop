ff5599a422d9c81c8e4b10ef8c998cc258d09738
pascal-vegt <9016712@student.zadkine.nl>
2024-06-15 10:27:01 +0200

    more styling home page

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-//<div class="main-categories-section-list-item">
-//<img class="main-categories-section-list-item-image" src="./images/566736.avif" alt="test image">
-//<h1 class="main-categories-section-list-item-title">Name</h1>
-//</div>
+`
+<div class="main-categories-section-list-item">
+<img class="main-categories-section-list-item-image" src="${category.imagepath}" alt="">
+<h1 class="main-categories-section-list-item-title">${category.name}</h1>
+</div>
+`
+    
+    
+    
+    
+    
+`
+<div class="main-brands-section-list-item">
+<img class="main-brands-section-list-item-image" src="${brands.imagepath}" alt="test image">
+</img><h1 class="main-brands-section-list-item-title">${brands.name}</h1>
+</div>
+`
+    
+    
+    
+    
+`
+<div class="main-seenbefore-section-list-item">
+<img class="main-seenbefore-section-list-item-image" src="${seenbefore.imagepath}" alt="test image">
+<h1 class="main-seenbefore-section-list-item-title">${seenbefore.name}</h1>
+</div>
+`
