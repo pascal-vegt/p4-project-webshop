0fcf3c7a68e767ee173bdaca345d0dc25b966148
pascal-vegt <9016712@student.zadkine.nl>
2024-06-19 12:35:38 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 `
-<div class="main-categories-section-list-item">
-<img class="main-categories-section-list-item-image" src="${category.imagepath}" alt="">
-<h1 class="main-categories-section-list-item-title">${category.name}</h1>
+<a class="main-first-section-list-item nodec" href="./category.html">
+<img class="main-first-section-list-item-image" src="${category.imagepath}" alt="">
+<h1 class="main-first-section-list-item-title">${category.name}</h1>
+</a>
+`
+    
+    
+    
+    
+    
+`
+<div class="main-second-section-list-item">
+<img class="main-second-section-list-item-image" src="${brands.imagepath}" alt="test image">
+</img><h1 class="main-second-section-list-item-title">${brands.name}</h1>
 </div>
 `
     
     
     
     
-    
 `
-<div class="main-brands-section-list-item">
-<img class="main-brands-section-list-item-image" src="${brands.imagepath}" alt="test image">
-</img><h1 class="main-brands-section-list-item-title">${brands.name}</h1>
-</div>
-`
-    
-    
-    
-    
-`
-<div class="main-seenbefore-section-list-item">
-<img class="main-seenbefore-section-list-item-image" src="${seenbefore.imagepath}" alt="test image">
-<h1 class="main-seenbefore-section-list-item-title">${seenbefore.name}</h1>
+<div class="main-third-section-list-item">
+<img class="main-third-section-list-item-image" src="${seenbefore.imagepath}" alt="test image">
+<h1 class="main-third-section-list-item-title">${seenbefore.name}</h1>
 </div>
 `
