40ec133315e63627bcce3776004b0c4a6f93d3e0
pascal-vegt <9016712@student.zadkine.nl>
2024-06-25 10:59:05 +0200

    bhilbwgrbhiwgrlbhiwgrjbwrwrjn

--- app.js
+++ app.js
@@ -4,24 +4,41 @@
 <h1 class="main-first-section-list-item-title">${category.name}</h1>
 </a>
 `
-    
-    
-    
-    
-    
+
+
+`
+<a class="main-first-section-list-item-alt2 nodec" href="./category.html">
+<img class="main-first-section-list-item-image-alt2" src="${category.imagepath}" alt="">
+<h1 class="main-first-section-list-item-title-alt2">${category.name}</h1>
+</a>
+`
+
+`
+<div class="main-first-section-list-item-alt">
+<img class="main-first-section-list-item-image-alt" src="${category.imagepath}" alt="">
+</div>
+`
+
+
 `
 <div class="main-second-section-list-item">
 <img class="main-second-section-list-item-image" src="${brands.imagepath}" alt="test image">
 </img><h1 class="main-second-section-list-item-title">${brands.name}</h1>
 </div>
 `
-    
-    
-    
-    
+
+
 `
 <div class="main-third-section-list-item">
 <img class="main-third-section-list-item-image" src="${seenbefore.imagepath}" alt="test image">
 <h1 class="main-third-section-list-item-title">${seenbefore.name}</h1>
+</div>
+`
+
+
+`
+<div class="brand-filter-group">
+<input class="brand-filter-checkbox" type="checkbox" name="brand.${category.brand}" id="${category.brand}" value="${category.brand}">
+<label class="brand-filter-text" for="${category.brand}">${category.brand}</label>
 </div>
 `
